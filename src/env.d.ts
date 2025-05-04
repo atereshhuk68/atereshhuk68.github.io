15e7d02c045e53f61ce64cb31204b38d2b236578
Oleksandr Tereshchuk <otereshhuk@proton.me>
2025-05-04 21:09:24 +0300

    feat: hide analytics scripts in dev env

---
+++ src/env.d.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export type {};
+
+declare global {
+	interface Window {
+		dataLayer?: object[];
+	}
+}
+
+// Astro types, not necessary if you already have a `tsconfig.json`
+/// <reference path="../.astro/types.d.ts" />
