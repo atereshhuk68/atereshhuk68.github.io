380866ef9aeb776b4aa5224d01c6c32de128400c
Oleksandr Tereshchuk <otereshhuk@proton.me>
2025-05-13 20:52:24 +0300

    feat: add scroll to feature via url

---
+++ src/assets/ts/scroll-to-via-url.ts
@@ -0,0 +1,21 @@
+export const scrollToViaUrl = (() => {
+	return {
+		init: () => {
+			const params = new URLSearchParams(window.location.search);
+			const scrollId = params.get('scroll');
+			if (scrollId) {
+				const el = document.getElementById(scrollId);
+				if (el) {
+					setTimeout(() => {
+						el.scrollIntoView({ behavior: 'smooth' });
+						params.delete('scroll');
+						const newUrl = window.location.pathname + (params.toString() ? '?' + params.toString() : '') + window.location.hash;
+						window.history.replaceState({}, '', newUrl);
+					}, 400);
+				}
+			} else {
+				console.warn('No scroll ID found in URL');
+			}
+		},
+	};
+})();
