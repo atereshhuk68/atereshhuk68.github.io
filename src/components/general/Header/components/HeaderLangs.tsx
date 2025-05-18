56dd9dcadbc93413a35fc825ebd18199034a2696
Oleksandr Tereshchuk <otereshhuk@proton.me>
2025-05-18 11:43:27 +0300

    fix: improve accessibility score

--- src/components/general/Header/components/HeaderLangs.tsx
+++ src/components/general/Header/components/HeaderLangs.tsx
@@ -3,16 +3,10 @@
  * Відображає випадаючий список мов для перемикання локалі.
  * @param {HeaderLangsProps} props - Список мов
  */
-import {
-	Button,
-	Dropdown,
-	DropdownItem,
-	DropdownMenu,
-	DropdownTrigger,
-} from "@heroui/react";
-import type { LanguagesListType } from "@i18n/langs";
-import { Languages } from "lucide-react";
-import type { ReactNode } from "react";
+import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from '@heroui/react';
+import type { LanguagesListType } from '@i18n/langs';
+import { Languages } from 'lucide-react';
+import type { ReactNode } from 'react';
 
 interface HeaderLangsProps {
 	items: LanguagesListType[];
@@ -22,11 +16,7 @@ export function HeaderLangs({ items }: HeaderLangsProps) {
 	return (
 		<Dropdown className="bg-black-100" shouldBlockScroll={false}>
 			<DropdownTrigger>
-				<Button
-					variant="light"
-					isIconOnly
-					startContent={<Languages size={24} />}
-				/>
+				<Button variant="light" isIconOnly startContent={<Languages size={24} />} aria-label="Open languages menu" />
 			</DropdownTrigger>
 			<DropdownMenu aria-label="Static Actions" items={items}>
 				{({ href, name }: LanguagesListType) => (
