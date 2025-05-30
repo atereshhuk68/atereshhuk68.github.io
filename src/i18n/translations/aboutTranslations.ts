9065ec5608222d703581ae67ce6201a206364f08
Oleksandr Tereshchuk <otereshhuk@proton.me>
2025-05-31 02:10:53 +0300

    feat: split translations to separate files

---
+++ src/i18n/translations/aboutTranslations.ts
@@ -0,0 +1,38 @@
+export const aboutTranslations = {
+	pl: {
+		'about.h2': 'Trochę o nas',
+		'about.p1':
+			'Nasze studio to coś więcej niż tylko miejsce usług kosmetycznych – to przestrzeń, w której artyzm spotyka się z pasją. Nasi doświadczeni profesjonaliści są oddani realizacji Twojej wizji, używając najwyższej jakości produktów i najnowszych innowacji kosmetycznych. Wejdź i pozwól nam sprawić, by Twoje piękno zabłysło!',
+		'about.p2':
+			'Pozostań w kontakcie z nami w mediach społecznościowych! Obserwuj nas, aby otrzymywać najnowsze aktualizacje, oferty specjalne i inspiracje. Odkryj kulisy i zobacz, dlaczego nasi klienci nas kochają. Dołącz do naszej rosnącej społeczności już dziś!',
+		'about.instagram': 'Subskrybuj nasz Instagram',
+		'about.other': 'Inne nasze media społecznościowe:',
+	},
+	en: {
+		'about.h2': 'A little about us',
+		'about.p1':
+			'Our studio is more than just a place for beauty services – it’s a space where artistry meets passion. Our experienced professionals are dedicated to bringing your vision to life, using top-quality products and the latest beauty innovations. Step in and let us make your beauty shine!',
+		'about.p2':
+			'Stay connected with us on social media! Follow us for the latest updates, special offers, and inspirations. Discover behind-the-scenes moments and see why our clients love us. Join our growing community today!',
+		'about.instagram': 'Subscribe on our Instagram',
+		'about.other': 'Other our social media:',
+	},
+	uk: {
+		'about.h2': 'Трохи про нас',
+		'about.p1':
+			'Наша студія - це більше, ніж просто місце для косметичних послуг – це простір, де майстерність зустрічається з пристрастю. Наші досвідчені професіонали прагнуть втілити ваше бачення в життя, використовуючи високоякісні продукти та останні інновації в галузі краси. Завітайте до нас і дозвольте нам підкреслити вашу красу!',
+		'about.p2':
+			"Залишайтеся на зв'язку з нами в соціальних мережах! Слідкуйте за нами, щоб отримувати останні оновлення, спеціальні пропозиції та натхнення. Дізнайтеся про закулісні моменти та подивіться, чому наші клієнти нас люблять. Приєднуйтесь до нашої зростаючої спільноти сьогодні!",
+		'about.instagram': 'Підпишіться на наш Instagram',
+		'about.other': 'Інші наші соціальні мережі:',
+	},
+	ru: {
+		'about.h2': 'Немного о нас',
+		'about.p1':
+			'Наша студия - это больше, чем просто место для косметических услуг – это пространство, где мастерство встречается со страстью. Наши опытные профессионалы посвящены воплощению вашего видения в жизнь, используя высококачественные продукты и последние инновации в области красоты. Заходите и позвольте нам подчеркнуть вашу красоту!',
+		'about.p2':
+			'Оставайтесь на связи с нами в социальных сетях! Следите за нами, чтобы получать последние обновления, специальные предложения и вдохновение. Узнайте о закулисных моментах и посмотрите, почему наши клиенты нас любят. Присоединяйтесь к нашему растущему сообществу сегодня!',
+		'about.instagram': 'Подпишитесь на наш Instagram',
+		'about.other': 'Другие наши социальные сети:',
+	},
+} as const;
