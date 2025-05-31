aa1268d7ca93f8614076cdfd934b0c2bd50a20be
Oleksandr Tereshchuk <otereshhuk@proton.me>
2025-06-01 00:04:19 +0300

    feat: add astro font api config for inter font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import react from "@astrojs/react";
 
@@ -25,5 +25,19 @@ export default defineConfig({
 		build: {
 			assetsInlineLimit: 0,
 		},
+	},
+	experimental: {
+		fonts: [
+			{
+				provider: fontProviders.fontsource(),
+				name: "Inter",
+				cssVariable: "--font-inter",
+				weights: ["100 900"],
+				subsets: ["latin", "latin-ext", "cyrillic", "cyrillic-ext"],
+				unicodeRange: [
+					"U+A,U+20,U+21,U+28,U+29,U+2C-3A,U+3F-47,U+49-50,U+52-57,U+5A,U+61-70,U+72-7A,U+F3,U+105,U+107,U+119,U+141,U+142,U+144,U+15A,U+15B,U+17A,U+17C,U+400-45F,U+2013,U+2116",
+				],
+			},
+		],
 	},
 });
