d1b9274a21f80f413aa7e3704a1dc8c4dc08d763
Oleksandr Tereshchuk <otereshhuk@proton.me>
2025-06-02 19:52:47 +0300

    fix: remove local cors and 200 status

--- public/send.php
+++ public/send.php
@@ -1,19 +1,4 @@
 <?php
-
-if (!function_exists('json_encode')) {
-    die('PHP JSON extension is not available');
-}
-
-header("Access-Control-Allow-Origin: http://localhost:4321");
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
-
 $response = array();
 
 $inputs = array(
