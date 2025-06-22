42b99efe4d4bbb96f5bb3301e5393ca5c395e747
Oleksandr Tereshchuk <otereshhuk@proton.me>
2025-06-22 14:52:24 +0300

    feat: add hero section slider

---
+++ src/i18n/translations/slidersTranslations.ts
@@ -0,0 +1,14 @@
+export const slidersTranslations = {
+	pl: {
+		imageAlt: 'Zdjęcie rezultatu',
+	},
+	en: {
+		imageAlt: 'Image of the result',
+	},
+	uk: {
+		imageAlt: 'Фото результату',
+	},
+	ru: {
+		imageAlt: 'Изображение результата',
+	},
+} as const;
