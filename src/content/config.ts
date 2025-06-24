cb09b202205c633451a03d14f4db7ef76bcfb0e5
Oleksandr Tereshchuk <otereshhuk@proton.me>
2025-06-24 23:29:39 +0300

    refactor: set collection right way

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,14 @@
-import { servicesCollectionsSchema } from '@/schemas/collections';
 import { defineCollection, z } from 'astro:content';
+import { servicesCollectionsSchema } from '@/schemas/collections';
+import { glob } from 'astro/loaders';
 
 export const collections = {
 	services: defineCollection({
-		type: 'data',
+		loader: glob({
+			pattern: '**/*.json',
+			base: 'src/content/services',
+			generateId: ({ entry }) => entry.replace(/\.json$/, ''),
+		}),
 		schema: z.array(servicesCollectionsSchema),
 	}),
 };
