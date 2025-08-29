e127bec4840b3b54befc75619e15512e16459125
Oleksandr Tereshchuk <otereshhuk@proton.me>
2025-08-29 22:05:57 +0300

    feat: get latest from master

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,13 @@
-import { defineCollection, z } from "astro:content";
-import { glob } from "astro/loaders";
-import { servicesCollectionsSchema } from "@/schemas/collections";
+import { servicesCollectionsSchema } from '@/schemas/collections';
+import { glob } from 'astro/loaders';
+import { defineCollection, z } from 'astro:content';
 
 export const collections = {
 	services: defineCollection({
 		loader: glob({
-			pattern: "**/*.json",
-			base: "src/content/services",
-			generateId: ({ entry }) => entry.replace(/\.json$/, ""),
+			pattern: '**/*.json',
+			base: 'src/content/services',
+			generateId: ({ entry }) => entry.replace(/\.json$/, ''),
 		}),
 		schema: z.array(servicesCollectionsSchema),
 	}),
